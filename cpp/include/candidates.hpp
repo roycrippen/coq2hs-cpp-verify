ecb2b258ac924dc2a9084c5a905528c963564fa5
roy.crippen <roy.crippen@archarithms.com>
2019-01-02 13:58:32 -0600

    simplify cpp code

--- cpp/include/candidates.hpp
+++ cpp/include/candidates.hpp
@@ -2,79 +2,44 @@
 
 using namespace std;
 
-namespace cn
-{
-int square(int a)
-{
-  return a * a;
-}
+namespace cn {
+    /// square a number
+    int square(int a) {
+        return a * a;
+    }
 
-bool is_pythagorean_triple(int a, int b, int c)
-{
-  return square(a) + square(b) == square(c);
-}
+    /// do a,b, c form a pythagorean triple
+    bool is_pythagorean_triple(int a, int b, int c) {
+        return square(a) + square(b) == square(c);
+    }
 
-// encode a string to a vector of ints
-vector<int> encode(string &cs)
-{
-  vector<int> res = {};
-  for (auto &c : cs)
-  {
-    res.push_back((int)c);
-  }
-  return res;
-}
+    /// XOR Encryption
+    string applyXorCipher(string const &cs, string const &key) {
+        string res = cs;
+        auto mod = key.size() / sizeof(char);
+        for (int i = 0; i < cs.size(); i++) {
+            auto c = key[i % mod];
+            res[i] = cs[i] ^ c;
+        }
+        return res;
+    }
 
-// decode an encoded vector of ints to a string
-string decode(vector<int> &xs)
-{
-  string s = "";
-  for (int x : xs)
-  {
-    s.push_back(x);
-  }
-  return s;
-}
+    /// pretty string of a const vector<T>
+    template<typename T>
+    string show(const vector<T> &vs) {
+        string res = "{ ";
+        for (int i = 0; i < vs.size() - 1; i++) {
+            res += to_string(vs[i]) + ", ";
+        }
+        res += to_string(vs[vs.size()]) + " }";
+        return res;
+    }
 
-// pretty string of a vector if int
-string show(const vector<int> &vs)
-{
-  string res = "{ ";
-  for (int i = 0; i < vs.size(); i++)
-  {
-    res += to_string(vs[i]) + ", ";
-  }
-  res.pop_back();
-  res.pop_back();
-  res += " }";
-  return res;
-}
-
-// pretty string of a vector if uint8_t
-string show(const std::vector<uint8_t> &vs)
-{
-  string res = "{ ";
-  for (int i = 0; i < vs.size(); i++)
-  {
-    res += to_string(vs[i]) + ", ";
-  }
-  res.pop_back();
-  res.pop_back();
-  res += " }";
-  return res;
-}
-
-// XOR Encryption
-string applyXorCipher(string const &cs, string const &key)
-{
-  string res = cs;
-  auto mod = key.size() / sizeof(char);
-  for (int i = 0; i < cs.size(); i++)
-  {
-    auto c = key[i % mod];
-    res[i] = cs[i] ^ c;
-  }
-  return res;
-}
+    /// pretty string of a vector<T>
+    template<typename T>
+    string show(vector<T> &vs) {
+        const vector<T> vs_ = vs;
+        return show(vs_);
+    }
 
 } // namespace cn
--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -1,38 +1,29 @@
 #include <iostream>
-#include <iomanip>
 #include "../include/candidates.hpp"
 
 using namespace std;
 
-int main(int argc, char *argv[])
-{
-  int a = 3;
-  int b = 4;
-  int c = 5;
-  cout << "Is (" << a << ", " << b << ", " << c << ") a pythagorean triple: "
-       << boolalpha << cn::is_pythagorean_triple(a, b, c) << "\n";
-  c = 6;
-  cout << "Is (" << a << ", " << b << ", " << c << ") a pythagorean triple: "
-       << boolalpha << cn::is_pythagorean_triple(a, b, c) << "\n";
+int main(int argc, char *argv[]) {
+    int a = 3;
+    int b = 4;
+    int c = 5;
+    cout << "Is (" << a << ", " << b << ", " << c << ") a pythagorean triple: "
+         << boolalpha << cn::is_pythagorean_triple(a, b, c) << "\n";
+    c = 6;
+    cout << "Is (" << a << ", " << b << ", " << c << ") a pythagorean triple: "
+         << boolalpha << cn::is_pythagorean_triple(a, b, c) << "\n";
 
-  // string cs = "This is the test string...";
-  // cout << "input string: '" << cs << "'\n";
-  // auto encoded_vec = cn::encode(cs);
-  // cout << "encoded vector: " << cn::show(encoded_vec) << "\n";
-  // auto ds = cn::decode(encoded_vec);
-  // cout << "decoded string: '" << ds << "'\n";
+    auto cs = "This is the test string...";
+    string key = "cipher key 123";
+    cout << "key: '" << key << "'\n";
+    cout << "input     string: '" << cs << "'\n";
+    auto encrypted = cn::applyXorCipher(cs, key);
+    const vector<uint8_t> vec(encrypted.begin(), encrypted.end());
+    cout << "encrypted string: '" << cn::show(vec) << "'\n";
+    auto decrypted = cn::applyXorCipher(encrypted, key);
+    cout << "decrypted string: '" << decrypted << "'\n";
+    auto orig_str = cn::applyXorCipher(cn::applyXorCipher(cs, key), key);
+    cout << "applyXorCipher(applyXorCipher(cs, key), key): '" << orig_str << "'\n";
 
-  auto cs = "c=Gon^#c#&y";
-  string key = "cipher key 123";
-  cout << "key: '" << key << "'\n";
-  cout << "input     string: '" << cs << "'\n";
-  auto encrypted = cn::applyXorCipher(cs, key);
-  const std::vector<uint8_t> vec(encrypted.begin(), encrypted.end());
-  cout << "encrypted string: '" << cn::show(vec) << "'\n";
-  auto decrypted = cn::applyXorCipher(encrypted, key);
-  cout << "decrypted string: '" << decrypted << "'\n";
-  auto orig_str = cn::applyXorCipher(cn::applyXorCipher(cs, key), key);
-  cout << "applyXorCipher(applyXorCipher(cs, key), key): '" << orig_str << "'\n";
-
-  return 0;
+    return 0;
 }
