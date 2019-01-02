6a390cc150c2c286e8bb12b931602f990f6ef261
roy.crippen <roy.crippen@archarithms.com>
2019-01-02 13:59:29 -0600

    cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -8,9 +8,11 @@ import           Control.Monad.IO.Class
 
 main :: IO ()
 main = do
+    putStrLn "\nInline CPP compared to Haskell\n"
+
+    -- example hs and cpp result compare
     cppSquare   <- CPP.square 3
     isCppTriple <- CPP.isTriple 3 4 5
-    putStrLn "Inline CPP compared to Haskell"
     putStrLn $ "CPP.square 3: " ++ show cppSquare
     putStrLn $ " HS.square 3: " ++ show (HS.square 3)
     putStrLn $ "CPP.isTriple 3 4 5: " ++ show isCppTriple
@@ -26,13 +28,15 @@ main = do
     putStrLn $ "rangeList 10 = " ++ show xs ++ "\n"
 
     -- validate calling CPP.applyXorCipher with std::cout
+    -- interesting way to quickly test the cpp code
     CPP.testApplyXorCipher
 
     -- CPP.applyXorCipher twice
     putStrLn ""
-    let s1 = "This is a test string ABC 123..."
+    let s1  = "String to cipher."
+    let key = "my secret key"
     putStrLn $ "input string   = " ++ show s1
-    s2 <- CPP.applyXorCipher s1
+    s2 <- CPP.applyXorCipher s1 key
     putStrLn $ "encoded string from cpp = " ++ show s2
-    s3 <- CPP.applyXorCipher s2
+    s3 <- CPP.applyXorCipher s2 key
     putStrLn $ "decoded string from cpp = " ++ show s3
