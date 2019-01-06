cdde1149b7ddcd7ae9bf26daf891f85e464a9428
roy.crippen <roy.crippen@archarithms.com>
2019-01-06 08:33:53 -0600

    added unicode to utf8 encoding and utf8 to unicode decoding

--- cpp/include/candidates.hpp
+++ cpp/include/candidates.hpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <sstream>
 
 using namespace std;
 
@@ -40,6 +41,87 @@ namespace cn {
     string show(vector<T> &vs) {
         const vector<T> vs_ = vs;
         return show(vs_);
+    }
+
+    wstring show(const uint32_t val) {
+        wstringstream ws;
+        ws << val;
+        wstring str;
+        ws >> str;
+        return str;
+    }
+
+    string show_utf8_hex(vector<uint8_t> xs) {
+        stringstream ss;
+        for (auto i = 0; i < xs.size() - 1; ++i) {
+            ss << uppercase << hex << static_cast<int>(xs[i]) << " ";
+        }
+        ss << uppercase << hex << static_cast<int>(xs[xs.size() - 1]);
+        auto s = ss.str();
+        if (s == "0")
+            s = "00";
+        return s;
+    }
+
+    // Unicode encoding and decoding
+    // port from https://rosettacode.org/wiki/UTF-8_encode_and_decode#Go
+    // first byte of a 2-byte encoding starts 110 and carries 5 bits of data
+    const uint8_t b2Lead = 0xC0;  // 1100 0000
+    const uint8_t b2Mask = 0x1F;  // 0001 1111
+
+    // first byte of a 3-byte encoding starts 1110 and carries 4 bits of data
+    const uint8_t b3Lead = 0xE0;  // 1110 0000
+    const uint8_t b3Mask = 0x0F;  // 0000 1111
+
+    // first byte of a 4-byte encoding starts 11110 and carries 3 bits of data
+    const uint8_t b4Lead = 0xF0;  // 1111 0000
+    const uint8_t b4Mask = 0x07;  // 0000 0111
+
+    // non-first bytes start 10 and carry 6 bits of data
+    const uint8_t mbLead = 0x80;  // 1000 0000
+    const uint8_t mbMask = 0x3F;  // 0011 1111
+
+
+    vector<uint8_t> encodeUTF8(const uint32_t i) {
+        vector<uint8_t> res = {};
+
+        if (i <= (1 << 7) - 1) { // max code point that encodes into a single byte
+            res.push_back(static_cast<uint8_t>(i));
+        } else if (i <= (1 << 11) - 1) {  // into two bytes
+            res.push_back(b2Lead | static_cast<uint8_t>(i >> 6));
+            res.push_back(mbLead | (static_cast<uint8_t>(i) & mbMask));
+        } else if (i <= (1 << 16) - 1) {  // three
+            res.push_back(b3Lead | static_cast<uint8_t>(i >> 12));
+            res.push_back(mbLead | (static_cast<uint8_t>(i >> 6) & mbMask));
+            res.push_back(mbLead | (static_cast<uint8_t>(i) & mbMask));
+        } else {  // four
+            res.push_back(b4Lead | static_cast<uint8_t>(i >> 18));
+            res.push_back(mbLead | (static_cast<uint8_t>(i >> 12) & mbMask));
+            res.push_back(mbLead | (static_cast<uint8_t>(i >> 6) & mbMask));
+            res.push_back(mbLead | (static_cast<uint8_t>(i) & mbMask));
+        }
+        return res;
+    }
+
+    uint32_t decodeUTF8(const vector<uint8_t> &bs) {
+        if (bs.size() < 1 || bs.size() > 4)
+            return 0;
+
+        auto bs0 = bs[0];
+        if (bs0 < 0x80)
+            return static_cast<uint32_t>(bs0);
+        else if (bs0 < 0xE0)
+            return static_cast<uint32_t>(bs0 & b2Mask) << 6 |
+                   static_cast<uint32_t>(bs[1] & mbMask);
+        else if (bs0 < 0xF0)
+            return static_cast<uint32_t>(bs0 & b3Mask) << 12 |
+                   static_cast<uint32_t>(bs[1] & mbMask) << 6 |
+                   static_cast<uint32_t>(bs[2] & mbMask);
+        else
+            return static_cast<uint32_t>(bs0 & b4Mask) << 18 |
+                   static_cast<uint32_t>(bs[1] & mbMask) << 12 |
+                   static_cast<uint32_t>(bs[2] & mbMask) << 6 |
+                   static_cast<uint32_t>(bs[3] & mbMask);
     }
 
 } // namespace cn
--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -25,5 +25,15 @@ int main(int argc, char *argv[]) {
     auto orig_str = cn::applyXorCipher(cn::applyXorCipher(cs, key), key);
     cout << "applyXorCipher(applyXorCipher(cs, key), key): '" << orig_str << "'\n";
 
+    const vector<uint32_t> codepoints = {0x0041, 0x00f6, 0x0416, 0x20ac, 0x1d11e, 0x0000};
+
+    cout << "\n";
+    printf("Unicode    UTF-8 encoding  Decoded  \n");
+    printf("---------  --------------  ---------\n");
+    for (auto codepoint : codepoints) {
+        auto utf8 = cn::encodeUTF8(codepoint);
+        auto decoded = cn::decodeUTF8(utf8);
+        printf("U+%-7.4x  %-14s  U+%-7.4x\n", codepoint, cn::show_utf8_hex(utf8).c_str(), decoded);
+    }
     return 0;
 }
